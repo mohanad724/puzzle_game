3d5359f2e5403023bdf2b74b9aba8d6f23e711c8
mohanad724 <85371078+mohanad724@users.noreply.github.com>
2023-01-01 14:33:54 +0300

    Add files via upload

---
+++ PuzzleSolver/solver/Test.py
@@ -0,0 +1,17 @@
+import Node
+import Solution
+import Heuristics
+
+
+initialNode = ((1,2,5),(3,4,0),(6,7,8))
+problem = Solution.Solution(initialNode)
+
+#problem.BFS()
+#problem.DFS()
+#problem.AStar(Heuristics.manhattanDistance)
+#problem.AStar(Heuristics.euclideanDistance)
+
+
+
+
+
